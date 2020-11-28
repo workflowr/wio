67c0eaf8758a0b9c6d7140d637fa271ed9152383
John Blischak <jdblischak@gmail.com>
2020-11-27 21:25:22 -0500

    Obtain list of GitHub repositories that installed the GitHub App.

---
+++ R/github.R
@@ -0,0 +1,40 @@
+#' Get list of workflowr projects hosted on GitHub
+#'
+#' export
+getProjectsOnGitHub <- function() {
+
+  appId <- Sys.getenv("GH_APP_ID")
+  if (nchar(appId) == 0) {
+    stop("Unable to find environment variable GH_APP_ID")
+  }
+  appKey <- Sys.getenv("GH_APP_KEY")
+  if (nchar(appKey) == 0) {
+    stop("Unable to find environment variable GH_APP_KEY")
+  }
+  if (!file.exists(appKey)) {
+    stop("The key file specified by GH_APP_KEY does not exist: %s",
+         appKey)
+  }
+  installations <- ghapps::gh_app_installation_list(
+    app_id = appId,
+    app_key = appKey
+  )
+  projects <- list()
+  for (install in installations) {
+    token <- ghapps::gh_app_token(
+      installation = install$account$login,
+      app_id = appId,
+      app_key = appKey
+    )
+    repositories <- gh::gh(
+      "/installation/repositories",
+      .token = token
+    )
+    for (repo in repositories$repositories) {
+      projects <- c(projects, list(list(owner = repo$owner$login,
+                                        repo = repo$name)))
+    }
+  }
+
+  return(projects)
+}
