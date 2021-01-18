99ed461748f140d5f224d8f3aeb80d88cd73b532
John Blischak <jdblischak@gmail.com>
2021-01-18 15:38:35 -0500

    Use TinyPNG API via tinieR to shrink images.
    optipng (via webshot::shrink) shrunk the screenshots by ~30%. TinyPNG
    shrinks them by ~70%.
    
    Had to request a developer key from TinyPNG and save it in env var
    TINY_API.
    
    https://tinypng.com/developers
    https://jmablog.com/post/tinier-release/
    https://github.com/jmablog/tinieR

--- R/screenshot.R
+++ R/screenshot.R
@@ -2,13 +2,15 @@
 #'
 #' @param websites character vector of URLs to websites
 #' @param thumbnails character vector of filenames for thumbnail images
+#' @param useTinyPng logical, Use the TinyPNG API to shrink the images. Requires
+#'   the environment variable \code{TINY_API} to be defined with your API key.
 #'
 #' @examples
 #'   screenshot("https://stephenslab.github.io/wflow-divvy/", "test.png")
 #'
 #' @return Character vector of filenames of thumbnail images (invisibly)
 #' @export
-screenshot <- function(websites, thumbnails) {
+screenshot <- function(websites, thumbnails, useTinyPng = TRUE) {
   if (length(websites) == 0) {
     return(invisible(character(0)))
   }
@@ -22,6 +24,27 @@ screenshot <- function(websites, thumbnails) {
     zoom = 2
   )
   webshot::resize(thumbnails, as.character(16 * 50))
-  webshot::shrink(thumbnails)
+  shrinkImage(thumbnails, useTinyPng = useTinyPng)
   return(invisible(thumbnails))
+}
+
+# Uses TinyPNG API or optipng (via webshot::shrink) to reduce file size
+shrinkImage <- function(image, useTinyPng = TRUE) {
+  if (useTinyPng && Sys.getenv("TINY_API") != "") {
+    # tinify() is not vectorized
+    for (file in image) {
+      tinieR::tinify(
+        file,
+        overwrite = TRUE,
+        quiet = FALSE
+      )
+    }
+    return(invisible(NULL))
+  }
+
+  # Send warning if unable to use TinyPNG
+  if (useTinyPng)
+    warning("Unable to use TinyPNG API. Requires env var TINY_API")
+
+  webshot::shrink(image)
 }
